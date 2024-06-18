d1d8c9494fcd58ca7dc73c6d7044e6b286c8edd9
Jorge Cata <jorggecata@gmail.com>
2024-06-18 02:35:43 +0200

    talwind-maquetado-evento rezise

--- practicas-react/src/Contenedor.jsx
+++ practicas-react/src/Contenedor.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import ProductosContainer from "./ProductosContainer";
 
 
 function Contenedor() {
 
+    const [isDesktop,setIsDesktop] = useState(false);
 
-    // muestra del componente
+
+    // window.addEventListener('resize',(event)=>{
+    //     console.log(event);
+    //     console.log('resize');
+    // })
+    window.addEventListener('resize',()=>{
+        // console.log();
+        // console.log('resize');
+        const ancho = window.innerWidth;
+        // console.log(ancho +' el ancho de mi pantalla');
+        if(ancho>1000){
+           setIsDesktop(true)
+        }else{
+            setIsDesktop(false);
+        }
+    })
     
     return (
         <>
--- practicas-react/src/Productos.jsx
+++ practicas-react/src/Productos.jsx
@@ -1,24 +1,22 @@
 
 
-// Aqui se muestran los datos que mando ProductosContainer
+
 
 function Productos({ productos }) {
-    // console.log(productos)
-    // productos.map(()=>{
-    //     console.log('productos')
-    // })
+    
 
     return (
         <>
             <section className="grid grid-cols-1 ms:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 xl:grid-cols-6">
                 {
                     productos.map((producto, indice) => {
-                        console.log(producto)
+                        // console.log(producto)
                         return (
-                            <article key={indice} className="bg-white text-black p-4 text-center rounded-md shadow-xl">
-                                <h2>{producto.title}</h2>
-                                <img src={producto.images[0]} alt={producto.title} />
-                                <p>$ {producto.price}</p>
+                            <article key={indice} className="bg-white text-black p-4 text-center rounded-md shadow-xl hover:scale-105 transition-all">
+                                <img src={producto.images[0]} alt={producto.title} className="aspect-square object-contain"/>
+                                <h2 className="font-bold truncate">{producto.title}</h2>
+                                <p className="text-gray-400">$ {producto.price}</p>
+                                <button className="bg-blue-300 rounded-sm text-black p-2  ">detalles</button>
                             </article>
                         )
                     })
